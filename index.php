a779a99b6df9778e2bd3b31aa4478627d7a82ed5
herbstluft <herbstluftwm.28@gmail.com>
2023-08-07 05:53:14 +0000

    base

--- index.php
+++ index.php
@@ -33,21 +33,19 @@
 
 <form method="POST" action="src/scripts/registrar.php" class="login">
 
-<h2 style="font-weight: 700; text-align:center">Te registras coachoncito? 
-<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="red" class="bi bi-hearts" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M4.931.481c1.627-1.671 5.692 1.254 0 5.015-5.692-3.76-1.626-6.686 0-5.015Zm6.84 1.794c1.084-1.114 3.795.836 0 3.343-3.795-2.507-1.084-4.457 0-3.343ZM7.84 7.642c2.71-2.786 9.486 2.09 0 8.358-9.487-6.268-2.71-11.144 0-8.358Z"/>
-</svg>
-</h2>
+<h2 style="font-weight: 700; text-align:center">Bienvenido <img width="40" height="40" src="https://img.icons8.com/emoji/48/waving-hand-emoji.png" alt="waving-hand-emoji"/> </h2>
+<h4 style="font-weight: 100;color: #575757bb; font-size:16px; text-align:center">Ingrese sus datos y registrese. </h4>
+
 <br>
 
 <div class="form-floating mb-3">
-  <input type="text" class="form-control input" id="floatingInput" name="nombre" placeholder="name@example.com">
+  <input type="text" class="form-control input" id="floatingInput" name="nombre" placeholder="name@example.com" required>
   <label style="color: #575757bb;" for="floatingInput">Nombre Completo</label>
 </div>
 
 
 <div class="form-floating mb-3">
-  <input type="email" class="form-control input" id="floatingInput" name="correo" placeholder="name@example.com">
+  <input type="email" class="form-control input" id="floatingInput" name="correo" placeholder="name@example.com" required>
   <label style="color: #575757bb;" for="floatingInput">Correo Electronico</label>
 </div>
 
--- src/scripts/registrar.php
+++ src/scripts/registrar.php
@@ -10,12 +10,20 @@ $correo = $_POST['correo'];
 // Validar y procesar los datos
 if (!empty($nombre) && !empty($correo)) {
 
-$sql = "INSERT INTO usuarios (nombre, email) VALUES ('$nombre', '$correo')";
-$db->ejecutarConsulta($sql);
- 
-header("Location: ../../index.php");
-exit();
-  }
+	//Verificar si ese usuario o correo existe
+	$sql = "SELECT * from usuarios where usuarios.nombre='$nombre' AND usuarios.email='$correo'";
+        $verificacion_nombre_correo = $db->seleccionarDatos($sql);
+
+	if(empty($verificacion_nombre_correo)){
+		$sql= "INSERT INTO usuarios (nombre, email) VALUES ('$nombre', '$correo')";
+		$db->ejecutarConsulta($sql);
+	}
+
+		header("Location: ../../index.php");
+		echo "Nombre o correo existente, elija otros porfavor";
+		exit();
+	
+}
 
 
 ?>
